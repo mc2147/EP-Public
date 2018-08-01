367f41e26e94fe7849e032f0513447bfa6dfdd41
mc2147 <matthewchan2147@gmail.com>
2018-08-01 05:52:51 -0500

    testing

--- script/modify-users.js
+++ script/modify-users.js
@@ -10,10 +10,20 @@ async function modifyUsers() {
     craigUser.workouts[1].Completed = true;
     craigUser.workouts[2].Completed = true;
     craigUser.changed('workouts', true);
-    await craigUser.save();
+    let wDates = craigUser.workoutDates;
+    let newDates = wDates.slice(0, -1);
+    let newFirstDateNum = wDates[0].getDate() - 3;
+    let newFirstDateObj = Object.assign({}, wDates[0]);
+    newFirstDateObj.setDate(newFirstDateNum);
+    newDates.unshift(newFirstDateObj);
+    console.log('calculating new dates: ', newDates);
+    for (let i = 0; i < newDates.length; i ++) {
+        console.log('   new date: ', newDates[i]);
+    }
     // for (var W in craigUser.workouts) {
 
     // }
+    await craigUser.save();
 
     // let benjamin = await User.findOne({
     //     where:{
