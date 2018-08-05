b742a370cd9ba3e1d28149aeec4a79280e469210
mc2147 <matthewchan2147@gmail.com>
2018-08-05 17:27:17 -0500

    testing

---
+++ api/admin.js
@@ -0,0 +1,44 @@
+// const session = require('express-session');
+import session from 'express-session';
+import Promise from "bluebird";
+var bodyParser = require('body-parser');
+var express = require('express');
+const bcrypt    = require('bcryptjs');
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
+import {getSubscriptionInfo} from './apiFunctions/stripeFunctions';
+import {signupUser, accessInfo} from './apiFunctions/userFunctions';
+import {assignWorkouts, assignLevel, getblankPatterns, rescheduleWorkouts} from './apiFunctions/workoutFunctions';
+import {generateHash} from './apiFunctions/userFunctions';
+import {updateSpecial} from './apiFunctions/workoutUpdate'
+import {generateWorkouts} from './apiFunctions/generateWorkouts';
+import {vueStats, getVueStat, vueProgress} from './vueFormat';
+import {LevelUpMesssages} from '../content/levelupMessages'
+import {sendMail, testEmail} from './email';
+var router = express.Router();
+import {Video, Exercise, WorkoutTemplate, SubWorkoutTemplate, Workout, User} from '../models';
+import moment from 'moment';
+var stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+console.log('secret key: ', process.env.STRIPE_SECRET_KEY)
+
+router.get('/testing', async function(req, res) {
+    res.json('testing admin route');
+})
+
+router.put('/videos/:id', async function(req, res) {
+    let inputs = req.body;
+    console.log('inputs: ', inputs);
+    Video.findOne({
+        where:{
+            id:req.params.id,
+        }
+    }).then(video => {
+        video.update(inputs);
+        video.save().then(savedVideo => {
+            res.json(savedVideo);
+        })
+    });
+})
+
+
+module.exports = router;
