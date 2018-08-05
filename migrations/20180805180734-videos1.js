c4200ce98a45e71613965e598f9a0a1b5b6d77b0
mc2147 <matthewchan2147@gmail.com>
2018-08-05 13:08:12 -0500

    migrations added

---
+++ migrations/20180805180734-videos1.js
@@ -0,0 +1,38 @@
+'use strict';
+
+// sequelize db:migrate will run "up functions in these migrations, one after the other"
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    // queryInterface is what changes database (queryInterface.addcolumn)
+    // Sequelize is the one we know (it has data types associated with it from when we define models)
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+    return queryInterface.addColumn(
+      'Video',
+      'exerciseCategory',
+      Sequelize.STRING,
+      {
+        defaultValue:''
+      }
+    )
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn(
+      'Video',
+      'exerciseCategory'
+    )
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
