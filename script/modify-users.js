f4315c4b492c2432c041cfa2c2dc550a370ffacd
mc2147 <matthewchan2147@gmail.com>
2018-08-17 09:57:33 -0500

    modify

--- script/modify-users.js
+++ script/modify-users.js
@@ -1,36 +1,72 @@
-const {CreateUser, SetUser} = require('../models/createUser');
-const {WorkoutTemplate, SubWorkoutTemplate, User, Video} = require('../models/index');
+const { CreateUser, SetUser } = require('../models/createUser');
+const { WorkoutTemplate, SubWorkoutTemplate, User, Video } = require('../models/index');
 
 async function modifyUsers() {
-    let craigUser = await User.findOne({
-        where:{
-            username:'c_lloy@hotmail.com'
+    let kevinUser = await User.findOne({
+        where: {
+            username: 'kholowac@mymts.net'
         }
     });
-    craigUser.workouts[1].Completed = true;
-    craigUser.workouts[2].Completed = true;
-    let wDates = craigUser.workoutDates;
-    let newDates = wDates.slice(0, -1);
-    let newFirstDateNum = wDates[0].getDate() - 3;
-    let newFirstDateObj = new Date(wDates[0]);
-    console.log('newFirstDateObj: ', newFirstDateObj);
-    
-    newFirstDateObj.setDate(newFirstDateNum);
-    newDates.unshift(newFirstDateObj);
-    console.log('calculating new dates: ', newDates);
-    craigUser.workoutDates = newDates;
-    for (let i = 0; i < newDates.length; i ++) {
-        let workoutIndex = i + 1;
-        console.log('   workoutIndex: ', workoutIndex, ' completed: ', craigUser.workouts[workoutIndex].Completed);
-        console.log('   new date: ', newDates[i]);
-        craigUser.workouts[workoutIndex].Date = newDates[i];
+    for (var W in kevinUser.workouts) {
+        if (W <= 3) {
+            kevinUser.workouts[W].Completed = true;
+        }
     }
-    // for (var W in craigUser.workouts) {
+    kevinUser.changed('workouts', true);
+    await kevinUser.save();
 
+
+    // craigUser.workouts[1].Completed = true;
+    // craigUser.workouts[2].Completed = true;
+    // let wDates = craigUser.workoutDates;
+    // let newDates = wDates.slice(0, -1);
+    // let newFirstDateNum = wDates[0].getDate() - 3;
+    // let newFirstDateObj = new Date(wDates[0]);
+    // console.log('newFirstDateObj: ', newFirstDateObj);
+
+    // newFirstDateObj.setDate(newFirstDateNum);
+    // newDates.unshift(newFirstDateObj);
+    // console.log('calculating new dates: ', newDates);
+    // craigUser.workoutDates = newDates;
+    // for (let i = 0; i < newDates.length; i ++) {
+    //     let workoutIndex = i + 1;
+    //     console.log('   workoutIndex: ', workoutIndex, ' completed: ', craigUser.workouts[workoutIndex].Completed);
+    //     console.log('   new date: ', newDates[i]);
+    //     craigUser.workouts[workoutIndex].Date = newDates[i];
     // }
-    craigUser.changed('workoutDates', true);
-    craigUser.changed('workouts', true);
-    await craigUser.save();
+    // craigUser.changed('workoutDates', true);
+    // craigUser.changed('workouts', true);
+
+    // await craigUser.save();    
+    // let craigUser = await User.findOne({
+    //     where:{
+    //         username:'c_lloy@hotmail.com'
+    //     }
+    // });
+    // craigUser.workouts[1].Completed = true;
+    // craigUser.workouts[2].Completed = true;
+    // let wDates = craigUser.workoutDates;
+    // let newDates = wDates.slice(0, -1);
+    // let newFirstDateNum = wDates[0].getDate() - 3;
+    // let newFirstDateObj = new Date(wDates[0]);
+    // console.log('newFirstDateObj: ', newFirstDateObj);
+
+    // newFirstDateObj.setDate(newFirstDateNum);
+    // newDates.unshift(newFirstDateObj);
+    // console.log('calculating new dates: ', newDates);
+    // craigUser.workoutDates = newDates;
+    // for (let i = 0; i < newDates.length; i ++) {
+    //     let workoutIndex = i + 1;
+    //     console.log('   workoutIndex: ', workoutIndex, ' completed: ', craigUser.workouts[workoutIndex].Completed);
+    //     console.log('   new date: ', newDates[i]);
+    //     craigUser.workouts[workoutIndex].Date = newDates[i];
+    // }
+    // // for (var W in craigUser.workouts) {
+
+    // // }
+    // craigUser.changed('workoutDates', true);
+    // craigUser.changed('workouts', true);
+    // await craigUser.save();
 
     // let benjamin = await User.findOne({
     //     where:{
@@ -48,13 +84,13 @@ async function modifyUsers() {
 }
 
 modifyUsers()
-  .catch(err => {
-    console.log("error!!!");
-    console.error(err.message)
-    console.error(err.stack)
-    process.exitCode = 1
-  })
-  .then(() => {
-    console.log('finished modifying users!!!')
-    process.exitCode = 0;
-  })
+    .catch(err => {
+        console.log("error!!!");
+        console.error(err.message)
+        console.error(err.stack)
+        process.exitCode = 1
+    })
+    .then(() => {
+        console.log('finished modifying users!!!')
+        process.exitCode = 0;
+    })
